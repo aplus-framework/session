81e9ed5e869853185da320c08ed20880951b6439
Natan Felles <natanfelles@gmail.com>
2021-06-05 14:04:52 -0300

    Add Redis tests

---
+++ tests/SaveHandlers/RedisTest.php
@@ -0,0 +1,20 @@
+<?php namespace Tests\Session\SaveHandlers;
+
+use Framework\Session\SaveHandlers\Redis;
+
+/**
+ * Class RedisTest.
+ *
+ * @runTestsInSeparateProcesses
+ */
+class RedisTest extends AbstractHandler
+{
+	public function setUp() : void
+	{
+		$this->config = [
+			'host' => \getenv('REDIS_HOST'),
+		];
+		$this->handler = new Redis($this->config, true, true);
+		parent::setUp();
+	}
+}
