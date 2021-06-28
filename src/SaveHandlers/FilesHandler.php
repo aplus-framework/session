0662887b55260960a25818710a04bff32a8a9fdb
Natan Felles <natanfelles@gmail.com>
2021-06-28 12:26:18 -0300

    Coding Standard: Add header comments

--- src/SaveHandlers/FilesHandler.php
+++ src/SaveHandlers/FilesHandler.php
@@ -1,4 +1,13 @@
-<?php namespace Framework\Session\SaveHandlers;
+<?php
+/*
+ * This file is part of The Framework Session Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\Session\SaveHandlers;
 
 use Framework\Log\Logger;
 use Framework\Session\SaveHandler;
--- tests/SaveHandlers/AbstractHandler.php
+++ tests/SaveHandlers/AbstractHandler.php
@@ -1,4 +1,13 @@
-<?php namespace Tests\Session\SaveHandlers;
+<?php
+/*
+ * This file is part of The Framework Session Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Session\SaveHandlers;
 
 use Framework\Log\Logger;
 use Tests\Session\SessionTest;
