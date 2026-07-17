434993f6931a88c453570c2ad1f25da1d7b34064
Natan Felles <natanfelles@gmail.com>
2026-07-17 19:55:20 -0300

    Allow User-Agent fallback to an empty string

--- src/SaveHandler.php
+++ src/SaveHandler.php
@@ -202,7 +202,7 @@ abstract class SaveHandler implements \SessionHandlerInterface, \SessionUpdateTi
      */
     protected function getUA() : string
     {
-        return $_SERVER['HTTP_USER_AGENT'];
+        return $_SERVER['HTTP_USER_AGENT'] ?? '';
     }
 
     protected function getKeySuffix() : string
